610d932a66b4f2be358545d249969b884f86eec1
RAFAEL FARIA BUENO <rafael.bueno@cs.udf.edu.br>
2026-09-17 11:10:58 -0300

    POGGGERRSS

--- CalculadoraLeal.py
+++ CalculadoraLeal.py
@@ -3,6 +3,19 @@ import os
 def limpar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+def soma(a, b):                #define a função de soma
+    return a + b
+
+def sub(a, b):
+    return a - b              #Define a função de subitração nessa porra
+
+def mult(a, b):
+    return a * b                #Define a função de multiplicação
+
+def div(a, b):
+    return a / b                     #Define a função divisão
+
 print("Bem vindo á Calculadora Leal")
 print("Para iniciarmos, digite 2 números inteiros:  ")
 
@@ -22,5 +35,7 @@ while True:
 
 limpar_terminal()
 
-print(f"{d1, d2} Perfeito!\nAgora, selecione uma das opções a baixo:")
+print(f"{d1, d2} Perfeito!")
+
+
 
